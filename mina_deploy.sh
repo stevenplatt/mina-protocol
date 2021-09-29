cd5f8eeefc216a5ef930e520b066a6ff0250b283
Steven Platt <mr.platt@gmail.com>
2021-09-28 17:24:37 -0700

    updated mina deploy shell script

---
+++ mina_deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#add the mina repository to the sources list and install mina keypair utility
+echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
+sudo apt-get update
+sudo apt-get install mina-generate-keypair=1.1.8-b10c0e3
+
+#create the required directories and permissions
+mkdir ~/keys
+chmod 700 ~/keys
+mina-generate-keypair -privkey-path ~/keys/my-wallet
