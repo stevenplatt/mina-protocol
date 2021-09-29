0600e16444cdbc8c58b01abea8c8391f0d6656a1
Steven Platt <mr.platt@gmail.com>
2021-09-28 18:06:26 -0700

    updated docker install script

---
+++ docker_install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# this script can be used to install docker to a linux host if it does not already exist
+# this script can be used to install Docker on top of a cloud hosted compute instance or a bare metal server
+# https://docs.docker.com/engine/install/ubuntu/
+
+# remove any pre-existing docker installations
+echo "Checking for and removing previous Docker installations..."
+sudo apt-get remove docker docker-engine docker.io containerd runc > docker_install.log
+
+echo "Checking for and installing Docker dependancies..."
+sudo apt-get update > /dev/null
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release >> docker_install.log
+
+echo "Installing the latest Docker release..."
+# add GPG key for HTTPS access to Docker repositories
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update > /dev/null
+# install Docker and related tools
+sudo apt-get install docker-ce docker-ce-cli containerd.io >> docker_install.log
+
+echo "Docker installation complete"
--- mina_keygen.sh
+++ mina_keygen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #add the mina repository to the sources list and install mina keypair utility
-echo "Updating repositories..."
+echo "Adding Mina repositories:"
 echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
-sudo apt-get update > /dev/null 2>&1
+sudo apt-get update > /dev/null
 
 echo "Installing the Mina keygen utility..."
 sudo apt-get install -y libjemalloc-dev > mina_kegen.log #this item is a required dependancy 
@@ -13,3 +13,6 @@ sudo apt-get install mina-generate-keypair=1.1.8-b10c0e3 >> mina_kegen.log
 mkdir ~/keys
 chmod 700 ~/keys
 mina-generate-keypair -privkey-path ~/keys/my-wallet
+
+#print final confirmation
+echo "Public/Private key pair generated sucessfully"
