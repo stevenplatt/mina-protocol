0a3184b8176e6bf3e3e02f9f9ce37449f2e46c9c
Steven Platt <mr.platt@gmail.com>
2021-09-28 17:38:28 -0700

    updated dependancies

--- mina_deploy.sh
+++ mina_deploy.sh
@@ -3,6 +3,7 @@
 #add the mina repository to the sources list and install mina keypair utility
 echo "deb [trusted=yes] http://packages.o1test.net release main" | sudo tee /etc/apt/sources.list.d/mina.list
 sudo apt-get update
+sudo apt-get install -y libjemalloc-dev #this item is a required dependancy 
 sudo apt-get install mina-generate-keypair=1.1.8-b10c0e3
 
 #create the required directories and permissions
