9eba1dd732e63a0f2251bdfb0c2437cd5a40526d
Steven Platt <mr.platt@gmail.com>
2021-09-28 18:08:44 -0700

    updated docker_install script

--- docker_install.sh
+++ docker_install.sh
@@ -23,8 +23,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update > /dev/null
+
 # install Docker and related tools
+sudo apt-get update > /dev/null
 sudo apt-get install docker-ce docker-ce-cli containerd.io >> docker_install.log
 
 echo "Docker installation complete"
