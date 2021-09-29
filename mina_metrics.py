66827e7ee054739e6a6d34ad23755c8bd66e06a8
Steven Platt <mr.platt@gmail.com>
2021-09-29 09:21:02 -0700

    updated arg parsing

--- mina_metrics.py
+++ mina_metrics.py
@@ -3,16 +3,33 @@
 # this script assumes that the Mina node is using network ports defined in mina_deploy.sh
 # the ip address of the mina node must be passed as an argument when running the sript
 
+# this script is run using the format "python mina_metrics.py [URL_TARGET]" 
+# example: "python mina_metrics.py http://127.0.0.1:8303/metrics"
+
 from flask import Flask
 from flask import render_template
 from prometheus_client.parser import text_string_to_metric_families
 import requests
+import argparse
+import os
+import sys
+
 app = Flask(__name__)
 
 @app.route("/")
 def metrics():
+
+    # Create the parser
+    my_parser = argparse.ArgumentParser(description='Collect and display Mina node metrics')
+
+    # Add the arguments
+    my_parser.add_argument('URL', metavar='url', type=str, help='mina node metrics url')
     
-    metrics = requests.get("http://localhost:8303/metrics").content
+    # Execute the parse_args() method
+    args = my_parser.parse_args()
+    target = args.URL
+    
+    metrics = requests.get(target).content
     dashboard = {}
 
     for family in text_string_to_metric_families(metrics):
