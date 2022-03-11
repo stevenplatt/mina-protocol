a470d8626991e53babedbc4be605201f095dc3d1
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2022-03-10 16:32:49 -0800

    New download missing blocks shell script

--- tools/dependency_search.py
+++ tools/dependency_search.py
@@ -22,30 +22,7 @@ def findcontainer(dir,keyword): # search all files in a directory for a specific
         #print(notice)
         pass
 
-# disabled for now, more work is needed to filter out IP addresses such as 127.0.0.1 and 0.0.0.0
-# https://www.geeksforgeeks.org/extract-ip-address-from-file-using-python/
-
-# https://github.com/MinaProtocol/mina/blob/develop/frontend/wallet/src/render/CodaProcess.re is a good example of static IP assignment
-
-def findip(dir): # search all files in a directory for static IP addresses
-  global ip_count
-  ip_pattern = re.compile(r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
-
-  for file in os.listdir(dir):
-    path = dir + "/" + file
-
-    try :
-        if os.path.isdir(path):
-          findip(path)
-        else:
-          if ip_pattern.search(open(path).read()):
-            print(path)
-            ip_count = ip_count +1
-    except Exception as notice:
-        #print(notice)
-        pass
-
-findcontainer("../mina", "gcr.io/o1labs-192920/")
+findcontainer("../mina", "Size.Small")
 print("This repository has " + str(container_count) + " container dependencies")
 
 # findip("../mina")
